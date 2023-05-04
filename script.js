b79261958baa6c563a5623a1e82ef3e411646a5a
unknown <sashkin1985>
2023-05-04 20:45:19 +0300

    js1

---
+++ script.js
@@ -0,0 +1,67 @@
+//1
+
+let uborshik = 40000;
+let voice_teacher = 22000;
+
+let uborshik_teacher;
+
+uborshik_teacher = uborshik + voice_teacher;
+
+console.log(uborshik_teacher);
+
+
+//2
+let vladivastok = "Владивосток";
+let two_thousand = "2000";
+
+console.log(vladivastok + " " + two_thousand);
+
+//3
+let messangers = 128;
+let photos = 254;
+let vk = 137;
+let youtube = 201;
+
+let all_time
+all_time = messangers + photos + vk + youtube;
+
+all_time = all_time / 60;
+console.log("Русалка просидела: " + all_time + " часов");
+
+//4
+let num = 2;
+let res;
+
+res = 2 ** 5;
+
+console.log(res);
+
+//5
+
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум в двое быстрее!");
+
+//6
+const greetings = "Привет";
+const name = "Кот";
+
+console.log(greetings + ", " + name)
+
+//7
+let time;
+time = 34;
+console.log('Старт поездки.Осталось минут: ' + time);
+time = time - 15;
+console.log('Немного сториз в соцсетях.Осталось минут:' + time);
+time = time - 10;
+console.log('Немного не новостей, но событий.Осталось минут:' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+//8
+let fahrenheit
+fahrenheit = 451;
+let celsius
+celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`);
+
+
